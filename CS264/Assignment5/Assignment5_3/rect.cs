f6e9491b63e9f5b18316b67692bcedd35c393d8f
AlexAmI123 <alex.majka.xxx@gmail.com>
2022-12-13 13:30:08 +0000

    Set up folders for assignment 5 using assignment 3

---
+++ CS264/Assignment5/Assignment5_3/rect.cs
@@ -0,0 +1,27 @@
+public class Rectangle
+{
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public string fill;
+    public string stroke;
+    public string strokeWidth;
+    public Rectangle(int x, int y, int width, int height, string fill, string stroke, string strokeWidth)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.fill = fill;
+        this.stroke = stroke;
+        this.strokeWidth = strokeWidth;
+    }
+    //add said shape to Memento
+    public void printShape(Caretaker caretaker)
+    {
+        string data = "<rect x = \""+ x +"\" y = \""+ y+"\" width = \""+width+"\" height = \""+ height +"\" style = \" fill : " + fill + " ; stroke : "+stroke+ " ; stroke-width : " + strokeWidth +"\" />";
+        Memento memento  = new Memento(data);
+        caretaker.addMemento(memento);
+    }
+}
